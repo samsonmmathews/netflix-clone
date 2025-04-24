73722098e1ed0d02f352c591232bb579392b4dc5
Samson Mathews <samsonmmathews@gmail.com>
2025-04-24 12:41:01 -0400

    initial setups

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const port = 5000
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://samsonmathew:pass1@cluster0.xusshfi.mongodb.net/netflix-clone', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', () => {
+    console.log("Connected to MongoDB");
+})
 
 app.listen(port, () => {
     console.log(`API is running on port ${port}`)
