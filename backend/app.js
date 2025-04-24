15a7ef64f0a99642bae4c03016efe7b581de8f3a
Samson Mathews <samsonmmathews@gmail.com>
2025-04-24 13:32:32 -0400

    Created database models for movies

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+app.set('view engine', 'hbs')
 const port = 5000
 
 const mongoose = require('mongoose');
---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    isAdmin: {
+        type: Boolean,
+        default: false //Default value is false for regular users
+    },
+})
+
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model;('User', userSchema);
+
+module.exports = User;
