6cce4b34dff4c41fe9311a333a3f950bc6386350
Samson Mathews <samsonmmathews@gmail.com>
2025-08-26 18:17:29 -0400

    Created Page to Search Movies

--- backend/routes/addMovie.js
+++ backend/routes/addMovie.js
@@ -18,13 +18,25 @@ router.post('/fetch-movie', async (req, res) => {
 
         const responseData = await fetch(url, options)
         const result = await responseData.json()
-        // console.log("Result: ", result)
-        res.json(result)
+
+        if(result.results.length ===0)
+        {
+            return res.status(404).json({ error: 'No movies found with the given search term' })
+        }
+
+        res.render('addMovieList', {movieList: result.results})
+
+        // res.json(result)
 
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to fetch movie details ' });
     }
+})
+
+router.get('/addMovie/:movieId', async (req, res) => {
+    const movieId = req.params.movieId;
+    res.json(movieId);
 })
 
 module.exports = router;
